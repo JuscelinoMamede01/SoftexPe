d26557021283672db90fd3c04b6634306b90362f
JuscelinoMamede <juscemamede01@gmail.com>
2023-08-31 11:31:56 -0300

    ex02Mod3 into online folder

---
+++ html/online/mod3/ex02/js/script.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".home-title ", { duration: 1000 });
+sr.reveal(".main-paragraph", {
+  origin: "top",
+  distance: "160px",
+  duration: 1000,
+  delay: 400,
+});
+sr.reveal(".h2-titles", { duration: 1000 });
+sr.reveal(".israel-flag", {
+  origin: "bottom",
+  distance: "160px",
+  duration: 1000,
+  delay: 400,
+});
+
+sr.reveal(".midias", { duration: 1000 });
